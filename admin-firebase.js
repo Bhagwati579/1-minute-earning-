822e9956cf490e2b8939b965ed8a11d55086aa05
Bhagwati579 <bhagwatilalpushkarna123@gmail.com>
2025-07-09 07:43:07 +0530

    Delete admin-firebase.js

--- admin-firebase.js
+++
@@ -1,9 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyCInD5L_WWTX9bD1sEOfkCdTt3sdGSeCo4",
-  authDomain: "earngati-gpay-system.firebaseapp.com",
-  projectId: "earngati-gpay-system",
-  storageBucket: "earngati-gpay-system.appspot.com",
-  messagingSenderId: "729473631351",
-  appId: "1:729473631351:web:354f01b5fe32a4fe843272",
-  measurementId: "G-7VT4TGL3N4"
-};
